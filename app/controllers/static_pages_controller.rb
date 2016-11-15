337d8d636964ebebb7ba7220451075178422a251
MariaFartusova <fartusova@uchi.ru>
2016-11-15 13:31:48 +0300

    Before action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,7 @@
 class StaticPagesController < ApplicationController
+  before_action :signed_in_user
   def home
-  	if signed_in?
-  		@questions = Question.all.paginate(page: params[:page])
-  		@question = current_user.questions.build
-    end
+	@questions = Question.all.paginate(page: params[:page])
+	@question = current_user.questions.build
   end
-end
-
+end	
