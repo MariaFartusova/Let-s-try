237a06435d4bc2898041a67bfd9dd633e57328bb
MariaFartusova <fartusova@uchi.ru>
2016-11-15 14:36:50 +0300

    User authenticate in sesssion controller

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,7 @@ module SessionsHelper
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
+    user.authenticate(params[:session][:password])
     self.current_user = user
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root  'static_pages#home'
   resources :users
   resources :sessions,   only: [:new, :create, :destroy]
-  resources :answers, only: [:create, :destroy]  
-  resources :questions, only: [:create, :destroy]  
+  resources :questions, only: [:create, :destroy] 
+  resources :answers, only: [:create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
