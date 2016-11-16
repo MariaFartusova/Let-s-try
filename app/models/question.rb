d2cad9730952621858b99003544ebb8cb2682b4b
MariaFartusova <fartusova@uchi.ru>
2016-11-16 17:03:09 +0300

    Css Files

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,9 @@
 class Question < ActiveRecord::Base
 	has_many :answers
+
 	belongs_to :user
-	default_scope -> { order('created_at DESC') }
+
 	validates :content, presence: true, length: { maximum: 140 }
+
     validates :user_id, presence: true
 end
