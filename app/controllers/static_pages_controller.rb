76a446925223ab74fe585ad3495e37cdff203912
MariaFartusova <fartusova@uchi.ru>
2016-12-01 13:35:36 +0300

    Sing in error

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   before_action :check_user_signed_in
-
+  
   def home
 	@questions = Question.all.paginate(page: params[:page])
 	@question = current_user.questions.build
